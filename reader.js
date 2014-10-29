5c01cc2e55c886c02db00787bdedbcce52e2ab56
GFury <gsamardjiev@gmail.com>
2014-10-29 13:22:03 +0200

    Initial

---
+++ reader.js
@@ -0,0 +1,37 @@
+var feed = require("feed-read");
+var http = require("http");
+var mongoose = require("mongoose")
+
+/*var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');*/
+
+
+
+feed("http://feeds.feedburner.com/RockPaperShotgun?format=xml", function(err, articles){
+    if (err) throw err;
+//    console.log(articles);
+    
+    var feeds =[];
+    for (var i=0; i<articles.length; i++){
+      feeds[i] = articles[i].title;
+    };
+    console.log(feeds.length);
+    var db = mongoose.connection;
+    db.on("error",console.error);
+    db.once("open",function(){
+      
+    });
+    
+    
+});
+
+
+// console.log(feeds)
+
+
+/*http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Only Testing\n');
+}).listen(3000, '0.0.0.0');
+*/
+
